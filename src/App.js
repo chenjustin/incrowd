d6d9989be0ab5ae0196f8dae8e5d5d182e474bad
Justin Chen <justinchen90@gmail.com>
2018-07-31 01:29:26 -0400

    Added normalize.css and react-infinite-scroller library. Also stylized the home page a bit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
 import React, { Component } from 'react';
-import Home from './Home/Home';
 import './App.css';
+import Article from './components/article/article';
+import InfiniteScroll from 'react-infinite-scroller';
+
 
 class App extends Component {
+  constructor() {
+    super();
+    fetch('https://www.stellarbiotechnologies.com/media/press-releases/json?limit=2&offset=1')
+      .then(
+        function(response) {
+          console.log(response);
+          if (response.status !== 200) {
+            console.log("There might have been a problem!");
+            return;
+          }
+
+          response.json().then(function(data) {
+            console.log(data);
+          });
+        }
+      )
+      .catch(function(err) {
+        console.log("Fetch error", err);
+      });
+  }
+
+
   render() {
     return (
-      <Home />
+      // <div style="height:700px;overflow:auto;">
+      //   <InfiniteScroll
+      //       pageStart={0}
+      //       loadMore={loadFunc}
+      //       hasMore={true || false}
+      //       loader={<div className="loader" key={0}>Loading ...</div>}
+      //       useWindow={false}
+      //   >
+      //       {items}
+      //   </InfiniteScroll>
+      // </div>
+      <div>Hello sir</div>
     );
   }
 }
